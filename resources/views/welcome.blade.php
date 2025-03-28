10739c4f69436068da99fb28ec0df5c42f4b6ea8
FabricioHD827 <168231311+FabricioHD827@users.noreply.github.com>
2025-03-27 19:48:36 -0500

    ultimo
    owo4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hola Mundillo Fabricio Huanca
                 </div>
 
                 <div class="links">
