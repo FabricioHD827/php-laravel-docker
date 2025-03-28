42f5360b5bb508f648846b439c01ec3c834f721d
FabricioHD827 <168231311+FabricioHD827@users.noreply.github.com>
2025-03-27 23:03:59 -0500

    commit5
    owo4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    HOla mundo
                 </div>
 
                 <div class="links">
